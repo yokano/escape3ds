a8090d5ee2158f9157e4e4963f0fd0b33e87ad2f
Yuta Okano <yuta.okano@gmail.com>
2013-07-26 04:18:35 +0900

    スクロールを促すメッセージをゲーム開始時に表示

--- client/runtime/runtime.js
+++ client/runtime/runtime.js
@@ -9,3 +9,10 @@ var rootView = new RootView({
 	model: state
 });
 $('body').append(rootView.render().el);
+
+// スクロールを促す
+game.get('message').show(['十字キーの↓を押して画面を合わせてください']);
+
+window.onscroll = function() {
+	alert('scroll');
+};
